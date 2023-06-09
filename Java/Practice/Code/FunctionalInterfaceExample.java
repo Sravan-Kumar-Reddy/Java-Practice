ea58fd340e943e51066d4004abdc3b602fc21975
Sravan Kumar Reddy Bontha <bonthasravan@gmail.com>
2023-06-09 08:20:15 +0530

    Functional Interfaces

--- Java/Practice/Code/FunctionalInterfaceExample.java
+++ Java/Practice/Code/FunctionalInterfaceExample.java
@@ -2,19 +2,19 @@ package Java.Practice.Code;
 
 @FunctionalInterface
 interface Addable{
-    int add(int a,int b);
+//    int add(int a,int b);
+
+    void add(int a, int b);
 }
 public class FunctionalInterfaceExample implements Addable{
 
-//    public static void main(String[] args) {
-        Addable addable = ((a, b) -> {
-            return a + b;
-        });
-//    }
+    static void sum(int a,int b){
+        System.out.println(a+b);
+    }
 
     @Override
-    public int add(int a, int b) {
+    public void add(int a, int b) {
         System.out.println(a+b);
-        return a+b;
+//        return a+b;
     }
 }
--- Java/Practice/DriverClass/DriverClass.java
+++ Java/Practice/DriverClass/DriverClass.java
@@ -3,20 +3,24 @@ package Java.Practice.DriverClass;
 import Java.Practice.Code.*;
 import jdk.nashorn.internal.ir.FunctionCall;
 
+import java.util.function.BiConsumer;
 
 
-@FunctionalInterface
-interface Addable{
-    int add(int a,int b);
-}
+//@FunctionalInterface
+//interface Addable{
+//    int add(int a,int b);
+//}
 public class DriverClass {
     public static void main(String[] args) {
         FunctionalInterfaceExample functionalInterfaceExample = new FunctionalInterfaceExample();
         functionalInterfaceExample.add(4,5);
-        Addable addable = (a,b)->{
-            return (a+b);
-        };
-        System.out.println(addable.add(9,9));
+//        Addable addable = (a,b)->{
+//            return (a+b);
+//        };
+//        System.out.println(addable.add(9,9));
+        BiConsumer<Integer,Integer> consumer = functionalInterfaceExample::add;
+        consumer.accept(10,10);
+
 
     }
 
